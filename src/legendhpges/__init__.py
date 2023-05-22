a36e03fbabf3ab0afc1af0bc9badc00fc3c0b41b
nikolay327 <111442458+nikolay327@users.noreply.github.com>
2023-05-22 18:03:04 +0200

    adding BEGe, PPC and SemiCoax detector geometry  (#5)

--- src/legendhpges/__init__.py
+++ src/legendhpges/__init__.py
@@ -1,4 +1,7 @@
 from ._version import version as __version__
+from .bege import BEGe
 from .invcoax import InvertedCoax
+from .ppc import PPC
+from .semicoax import SemiCoax
 
-__all__ = ["__version__", "InvertedCoax"]
+__all__ = ["__version__", "InvertedCoax", "BEGe", "PPC", "SemiCoax"]
