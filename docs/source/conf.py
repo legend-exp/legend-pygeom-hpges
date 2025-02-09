0a77fccac99562709596eecef535815676c452ab
Manuel Huber <info@manuelhu.de>
2025-02-09 14:55:12 +0100

    use dbetto for AttrsDict and TextDB

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ intersphinx_mapping = {
     "matplotlib": ("https://matplotlib.org/stable", None),
     "pint": ("https://pint.readthedocs.io/en/stable", None),
     "pyg4ometry": ("https://pyg4ometry.readthedocs.io/en/stable", None),
-    "legendmeta": ("https://pylegendmeta.readthedocs.io/en/stable/", None),
+    "dbetto": ("https://dbetto.readthedocs.io/en/stable/", None),
 }  # add new intersphinx mappings here
 
 # sphinx-autodoc
